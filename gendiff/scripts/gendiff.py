932e11f42c265a4d7b67022af9b833d473256a72
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2022-02-19 23:41:45 +1000

    feat: step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,11 +5,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
     parser.add_argument(
-        "first_file",
-    )
-    parser.add_argument(
-        "second_file",
+        "-f",
+        "--format",
+        default="json",
+        help="set format of output",
     )
 
     parser.parse_args()
